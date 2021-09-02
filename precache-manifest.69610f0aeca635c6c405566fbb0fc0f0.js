aa38b80af4e5ba6fdb2b53c7f199358a7384edc7
Ankit-Gupta4502 <83642182+Ankit-Gupta4502@users.noreply.github.com>
2021-09-02 00:47:01 -0400

    Updates

---
+++ precache-manifest.69610f0aeca635c6c405566fbb0fc0f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb4ec00288c9320cc613d7493578ab5",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "829ddb4e1a1c4492451d",
+    "url": "/quiz-app/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "56f4e72c80b3e3760a06",
+    "url": "/quiz-app/static/js/2.cb58932c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.cb58932c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829ddb4e1a1c4492451d",
+    "url": "/quiz-app/static/js/main.9b60f496.chunk.js"
+  },
+  {
+    "revision": "2b8e60fa09f000520361",
+    "url": "/quiz-app/static/js/runtime-main.d5202142.js"
+  }
+]);
